f9b82c95058b074942828f44f15dfb7cd1f0364e
shuvo18103107 <18103107@iubat.edu>
2020-11-15 22:05:51 +0600

    🔥 random word package executing

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 import 'dart:math';
 
 void main() {
@@ -12,10 +13,12 @@ class Randomnumber extends StatefulWidget {
 
 class _RandomnumberState extends State<Randomnumber> {
   int _number = 1;
+  var _word;
 
   void _randomnumbergenerator() {
     setState(() {
       _number = Random().nextInt(100) + 1;
+      _word = WordPair.random();
     });
   }
 
@@ -30,16 +33,31 @@ class _RandomnumberState extends State<Randomnumber> {
             child: Column(
           children: <Widget>[
             Expanded(
+                flex: 5,
                 child: Center(
                     child: Text(
-              'Random Number : $_number',
-              style: TextStyle(
-                color: Colors.green,
-                fontSize: 25,
-                fontStyle: FontStyle.italic,
-                fontWeight: FontWeight.bold,
+                  'Random Number : $_number',
+                  style: TextStyle(
+                    color: Colors.green,
+                    fontSize: 25,
+                    fontStyle: FontStyle.italic,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ))),
+            Expanded(
+              flex: 5,
+              child: Center(
+                child: Text(
+                  'Company Name : $_word',
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 25,
+                    fontStyle: FontStyle.italic,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
               ),
-            ))),
+            )
           ],
         )),
         floatingActionButton: FloatingActionButton(
